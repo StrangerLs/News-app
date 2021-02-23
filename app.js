3d9262d25bcc8ae6c62edda507d022a48fdc8aca
paulmoschetti <paulmoschetti93@gmail.com>
2021-02-23 15:31:18 -0700

    resolved cors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Endpoint Data
-const DOMAIN = 'http://newsapi.org/';
+const DOMAIN = 'http://cors-anywhere.herokuapp.com/http://newsapi.org/';
 const apiKey = '590e2f3ad3a7403499ccbcd8a5986d5f';
 const standardUrl = `${DOMAIN}v2/top-headlines?apiKey=${apiKey}`
 
@@ -12,7 +12,7 @@ async function newsWorks(value) {
 
   try {
     let response = await axios.get(news);
-    // console.log(response.data.articles);
+    console.log(response.data.articles);
 
     for (let i = 0; i < response.data.articles.length; i++) {
       console.log(response.data.articles[i]);
