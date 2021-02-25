e09ec51f70d255b6ae80eac26703abb744c3aeb4
paulmoschetti <paulmoschetti93@gmail.com>
2021-02-25 16:18:49 -0700

    cleanup

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-// Endpoint Data
+
 const DOMAIN = 'http://newsapi.org/';
 const apiKey = '590e2f3ad3a7403499ccbcd8a5986d5f';
 const standardUrl = `${DOMAIN}v2/top-headlines?apiKey=${apiKey}`
 
 
 
-// Async function/axios 
+
 async function newsWorks(value) {
   let news = `${standardUrl}&q=${value}`;
   
 
   try {
     let response = await axios.get(news);
-    console.log(response.data.articles);
+    
 
     for (let i = 0; i < response.data.articles.length; i++) {
       showNews(response.data.articles[i]);
@@ -26,7 +26,7 @@ async function newsWorks(value) {
 
 
 
-// making each article a list element. need to add image to each li element and append
+
 
 function showNews(data) {
   let articles = document.querySelector('.urls')
@@ -47,7 +47,7 @@ function showNews(data) {
 
 
 
-// event handler function
+
 let btn = document.querySelector('#push')
 btn.addEventListener('submit', (e)=> {
   e.preventDefault();
@@ -57,7 +57,7 @@ btn.addEventListener('submit', (e)=> {
 })
 
 
-// removing info
+
 function removeNews() {
   const remove = document.querySelector('.urls');
   while (remove.lastChild) {
